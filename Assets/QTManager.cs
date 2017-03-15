5247fb4f8f5b2884f3d67029d1f05db2ce499f99
Haoran Liu <ymiku@qq.com>
2017-03-15 15:20:50 +0800

    terrain

---
+++ Assets/QTManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QTManager : UnitySingleton<QTManager> {
+	public Transform playerTrans;
+	public TerrainManager activeTerrain;
+	public float backBuffer = 0.1f;
+	List<QTNode> nodeList;
+	QTNode tNode;
+	// Use this for initialization
+	public void Awake()
+	{
+		Init ();
+	}
+	public void Init()
+	{
+		playerTrans = GameObject.Find ("Player").transform;
+		activeTerrain.Init ();
+	}
+	// Update is called once per frame
+	public void Update()
+	{
+		Execute ();
+	}
+	public void Execute()
+	{
+		if (activeTerrain == null)
+			return;
+		activeTerrain.Execute ();
+		activeTerrain.TryGenerateBorder ();
+		activeTerrain.CalculateMesh ();
+		activeTerrain.UpdateMesh ();
+	}
+	public bool CanGenerate(QTNode node)
+	{
+		return MathExtra.GetV3L (QTManager.Instance.playerTrans.position - node.center) / node.length < QTManager.Instance.activeTerrain.cl;
+	}
+	public bool NeedBack(QTNode node)
+	{
+		return MathExtra.GetV3L (QTManager.Instance.playerTrans.position - node.center) / node.length >= (QTManager.Instance.activeTerrain.cl+backBuffer);
+	}
+	public QTNode FindBorder(QTNode node,int dir)
+	{
+		nodeList = null;
+		tNode = null;
+		for (int i = node.lodLevel+2; i < activeTerrain.maxLodLevel; i++) {
+			nodeList = activeTerrain.activeNodeListArray [i];
+			for (int m = 0; m < nodeList.Count; m++) {
+				tNode = nodeList[m];
+				switch (dir) {
+				case 0:
+					if (MathExtra.ApproEquals (node.center.z + node.length * 0.5f, tNode.center.z - tNode.length * 0.5f) &&
+					   (node.center.x > tNode.center.x - tNode.length * 0.5f) && (node.center.x < tNode.center.x + tNode.length * 0.5f))
+						return tNode;
+					break;
+				case 1:
+					if (MathExtra.ApproEquals (node.center.x + node.length * 0.5f, tNode.center.x - tNode.length * 0.5f) &&
+						(node.center.z > tNode.center.z - tNode.length * 0.5f) && (node.center.z < tNode.center.z + tNode.length * 0.5f))
+						return tNode;
+					break;
+				case 2:
+					if (MathExtra.ApproEquals (node.center.z - node.length * 0.5f, tNode.center.z + tNode.length * 0.5f) &&
+						(node.center.x > tNode.center.x - tNode.length * 0.5f) && (node.center.x < tNode.center.x + tNode.length * 0.5f))
+						return tNode;
+					break;
+				case 3:
+					if (MathExtra.ApproEquals (node.center.x - node.length * 0.5f, tNode.center.x + tNode.length * 0.5f) &&
+						(node.center.z > tNode.center.z - tNode.length * 0.5f) && (node.center.z < tNode.center.z + tNode.length * 0.5f))
+						return tNode;
+					break;
+				}
+			}
+		}
+		return null;
+	}
+}
---
+++ Assets/RootNode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RootNode : QTNode {
+	public RootNode(float length)
+	{
+		this.parent = null;
+		this.lodLevel = QTManager.Instance.activeTerrain.maxLodLevel;
+		this.quadrantID = 0;
+		this.length = length;
+		this.center = Vector2.zero;
+		borderStatus = BorderStatus.UpBorder | BorderStatus.RightBorder | BorderStatus.DownBorder | BorderStatus.LeftBorder;
+		CheckForLOD ();
+	}
+	public override void Destroy()
+	{
+		parent = null;
+	}
+}
